659d96e2b438782e91e7cb257df60ea74fb32800
Andersontbrand <andersonbteixeira@outlook.com>
2024-01-30 18:56:46 -0300

    update-sequelize-config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "postgres",
-  database: "codeburger",
+  url: 'postgresql://postgres:NuiRsQtKvkGVH2Mh9hzk@containers-us-west-116.railway.app:8052/railway',
   define: {
     timespamps: true,
     underscored: true,
